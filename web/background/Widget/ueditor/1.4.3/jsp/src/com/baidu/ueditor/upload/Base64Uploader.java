89ac6ce03277364f824f8c2ea1436153e314b91e
zhangweikg <403524979@qq.com>
2018-09-03 14:43:11 +0800

    第一次commit

--- web/background/Widget/ueditor/1.4.3/jsp/src/com/baidu/ueditor/upload/Base64Uploader.java
+++ web/background/Widget/ueditor/1.4.3/jsp/src/com/baidu/ueditor/upload/Base64Uploader.java
@@ -13,20 +13,20 @@ import org.apache.commons.codec.binary.Base64;
 public final class Base64Uploader {
 
 	public static State save(String content, Map<String, Object> conf) {
-		
+
 		byte[] data = decode(content);
 
 		long maxSize = ((Long) conf.get("maxSize")).longValue();
 
 		if (!validSize(data, maxSize)) {
-			return new BaseState(false, AppInfo.MAX_SIZE);
+//			return new BaseState(false, AppInfo.MAX_SIZE);
 		}
 
 		String suffix = FileType.getSuffix("JPG");
 
 		String savePath = PathFormat.parse((String) conf.get("savePath"),
 				(String) conf.get("filename"));
-		
+
 		savePath = savePath + suffix;
 		String physicalPath = (String) conf.get("rootPath") + savePath;
 
@@ -48,5 +48,5 @@ public final class Base64Uploader {
 	private static boolean validSize(byte[] data, long length) {
 		return data.length <= length;
 	}
-	
+
 }
